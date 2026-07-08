a1c85960d69a9d9a1885922a9649febf149fc353
unemotioned <134937260+unemotioned@users.noreply.github.com>
2026-07-08 13:36:37 +0900

    remove: pom.xml - mysql dependencie & mysql queries

--- database/mysql/root.sql
+++
@@ -1,9 +0,0 @@
--- '%': allows user to connect from any host
--- or can be replaced by specific ip or hostname(localhost || 192.168.0.43)
-create user 'spring_boot_template' @'%' identified by '1234';
-
--- grant all privileges on the database to the user
-grant all privileges on spring_boot_template.* to 'spring_boot_template' @'%';
-
--- to ensure changes are applied immediately
-flush privileges;
